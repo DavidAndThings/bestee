74b3539e0b3643eb891499bf11057ccd29f8f2f1
Xiao Wang <blazingd@Xiaos-Laptop.local>
2024-06-16 21:41:22 -0400

    added logic unit to search for stored entities

--- core/exp.go
+++ core/exp.go
@@ -2,56 +2,35 @@ package core
 
 type Expression struct {
 	header string
-	data   []interface{}
+	data   map[string]interface{}
 }
 
-func (exp *Expression) Evaluate(machine *Machine) {
+func (exp Expression) Evaluate(machine *Machine) {
 
-}
-
-type Pair struct {
-	key   string
-	value interface{}
 }
 
 type ExpressionArray struct {
-	data []*Expression
+	data []Expression
 }
 
 func NewExpressionArray() *ExpressionArray {
-	return &ExpressionArray{data: make([]*Expression, 0)}
+	return &ExpressionArray{data: make([]Expression, 0)}
+}
+
+func (array *ExpressionArray) IsEmpty() bool {
+	return len(array.data) == 0
+}
+
+func (array *ExpressionArray) Pop() Expression {
+	node := array.data[0]
+	array.data = array.data[1:]
+	return node
+}
+
+func (array *ExpressionArray) Add(newData ...Expression) {
+	array.data = append(array.data, newData...)
 }
 
 func (array *ExpressionArray) Size() int {
 	return len(array.data)
-}
-
-func (array *ExpressionArray) GetAt(index int) *Expression {
-	return array.data[index]
-}
-
-func (array *ExpressionArray) Add(newData ...*Expression) {
-	array.data = append(array.data, newData...)
-}
-
-type ExpressionQueue struct {
-	data []*Expression
-}
-
-func NewExpressionQueue() *ExpressionQueue {
-	return &ExpressionQueue{data: make([]*Expression, 0)}
-}
-
-func (queue *ExpressionQueue) IsEmpty() bool {
-	return len(queue.data) == 0
-}
-
-func (queue *ExpressionQueue) Pop() *Expression {
-	node := queue.data[0]
-	queue.data = queue.data[1:]
-	return node
-}
-
-func (queue *ExpressionQueue) Add(newData ...*Expression) {
-	queue.data = append(queue.data, newData...)
 }
